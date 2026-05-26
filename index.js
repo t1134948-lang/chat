97c351c0a583549d430cc9e5f61058f37c7edda9
t1134948-lang <t1134948@gmail.com>
2026-05-26 08:45:20 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '8907944426:AAEyvzbUHW3avJSRCttknvBNQh_HZ1jpOWc'; 
+const bot = new TelegramBot(token, {polling: true});
+
+let waitingUser = null;
+let pairs = {};
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Anonim chatga xush kelibsiz! /search tugmasini bosing.");
+});
+
+bot.onText(/\/search/, (msg) => {
+    const chatId = msg.chat.id;
+    if (waitingUser && waitingUser !== chatId) {
+        pairs[chatId] = waitingUser;
+        pairs[waitingUser] = chatId;
+        bot.sendMessage(chatId, "Suhbatdosh topildi! Yozishni boshlang.");
+        bot.sendMessage(waitingUser, "Suhbatdosh topildi! Yozishni boshlang.");
+        waitingUser = null;
+    } else {
+        waitingUser = chatId;
+        bot.sendMessage(chatId, "Suhbatdosh qidirilmoqda...");
+    }
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (pairs[chatId]) {
+        bot.sendMessage(pairs[chatId], msg.text);
+    }
+});
